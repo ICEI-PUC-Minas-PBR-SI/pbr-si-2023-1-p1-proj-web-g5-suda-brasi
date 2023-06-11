7c87f2c98d7bae8a41ffe2f1c0a177fd09e449c2
Nicolas Henrique <nicolasfonseca5@hotmail.com>
2023-06-11 03:24:42 -0300

    Update Geral dos codigos
    Update pós avaliação das paginas

--- Codigos/Tela-Mensagens/index.js
+++ Codigos/Tela-Mensagens/index.js
@@ -1,4 +1,16 @@
+<<<<<<< Updated upstream
 $('#searsh').on('input', function () {
+=======
+// Barra Lateral
+let toggle = document.querySelector('.toggle');
+let sideBar = document.querySelector('.sidebar-container');
+
+toggle.addEventListener('click', () => {
+  sideBar.classList.toggle('toggle-function');
+});
+
+$('#search').on('input', function () {
+>>>>>>> Stashed changes
     const val = $(this).val().toUpperCase()
     $('.posts-title').each(function () {
         if ($(this).html().toUpperCase().indexOf(val) !== -1) {
@@ -8,4 +20,8 @@ $('#searsh').on('input', function () {
             $(this).hide()
         }
     })
+<<<<<<< Updated upstream
 })
+=======
+})
+>>>>>>> Stashed changes
