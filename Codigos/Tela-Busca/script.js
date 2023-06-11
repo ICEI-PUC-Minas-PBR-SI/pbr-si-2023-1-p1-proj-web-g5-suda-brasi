a168eef7656db6cfc76ca1b66205c1caa6014dfc
Nicolas Henrique <105471632+Okitemuri@users.noreply.github.com>
2023-06-11 03:05:55 -0300

    Delete Codigos directory

--- Codigos/Tela-Busca/script.js
+++
@@ -1,6 +0,0 @@
-let toggle = document.querySelector('.toggle');
-let sideBar = document.querySelector('.sidebar-container');
-
-toggle.addEventListener('click', () => {
-  sideBar.classList.toggle('toggle-function');
-});
