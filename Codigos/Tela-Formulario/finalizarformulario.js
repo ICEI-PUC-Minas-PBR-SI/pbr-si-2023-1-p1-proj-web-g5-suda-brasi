f58afbd85ed9d5bcd122a501ad016f88e8d01234
GabrielValgas <gabrielsantosre98@gmail.com>
2023-06-26 16:45:09 -0300

    atualização

--- Codigos/Tela-Formulario/finalizarformulario.js
+++ Codigos/Tela-Formulario/finalizarformulario.js
@@ -17,17 +17,15 @@ function finalizar(){
     var numerodependentes = document.getElementById('input-NumeroDependentes').value;
     var profissao = document.getElementById('input-Profissão').value;
 
-    var usuarios = (JSON.parse(localStorage.getItem('db_usuario'))).dados;
-
-    usuarios.dados[usuarios.length] = {
-        id:usuarios.length.id,
+    usuarios.dados[usuarios.dados.length] = {
+        id:usuarios.dados.length.id,
         nome: nome,
         data: data,
         telefone: telefone,
         cpf: cpf,
         rg: rg,
-        email: usuarios.length.email,
-        senha: usuarios.length.senha,
+        email: usuarios.dados.length.email,
+        senha: usuarios.dados.length.senha,
         categoria:"",
         tipo:"",
         imagem:"",
@@ -47,17 +45,25 @@ function finalizar(){
     }
     console.log(usuarios)
     localStorage.setItem('db_usuario',JSON.stringify(usuarios));
+
+    document.getElementById('input-nomeCompleto').value = ""; 
+    document.getElementById('input-nascimento').value = "";
+    document.getElementById('input-telefone').value = "";
+    document.getElementById('input-cpf').value = "";
+    document.getElementById('input-rg').value = "";
+    document.getElementById('input-email').value = "";
+    document.getElementById('estadocivil').value = "";
+    document.getElementById('input-Rua').value = "";
+    document.getElementById('input-Número').value = "";
+    document.getElementById('input-Bairro').value = "";
+    document.getElementById('input-CEP').value = "";
+    document.getElementById('input-Estado').value = "";
+    document.getElementById('input-Cidade').value = "";
+    document.getElementById('input-RendaMensal').value = "";
+    document.getElementById('input-NumeroDependentes').value = "";
+    document.getElementById('input-Profissão').value = "";
+    document.getElementById('input-Complemento').value - "";
+
 }
-$(".enviar").on('click',handleFileSelect)
-function handleFileSelect(event) {
-    var file = event.target.files[0];
-    var reader = new FileReader();
-  
-    reader.onload = function() {
-      var image = new Image();
-      image.src = reader.result;
-      document.body.appendChild(image);
-    };
-  
-    reader.readAsDataURL(file);
-  }
+var botaoenviar = document.getElementById('botaoEnviar');
+botaoenviar.addEventListener('click', finalizar);
