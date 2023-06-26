26f3697f8dca1f879b6ee04ca2eaa57110cc731c
Rafaelclopes25 <1449825@sga.pucminas.br>
2023-06-26 19:38:30 -0300

    at5

--- Codigos/Tela Card/card.js
+++ Codigos/Tela Card/card.js
@@ -1,21 +1,23 @@
-var id = location.search.slice(1).split('=')
-var usuarios = (JSON.parse(localStorage.getItem('db_usuario'))).dados
-var atual;
-
-usuarios.forEach(element => {
-    if(element.id == id){
-        atual= element
-    }
-});
-
-$('.titulos').append(`${atual.nome}`)
-$('.imagem-perfil'.append)(`
-    <img src="${atual.pictures.medium}" class="imagem-usuario">
-`)
-$('#detalhes').append(`
-Categoria: ${atual.categoria}`)
-$("#telefone").append(`
-Contato: ${atual.telefone}`)
-$("#email").append(`
-    Email: ${atual.email}
-`)
+function GeraPagina(){
+    var id = location.search.slice(1).split('=')
+    var usuarios = (JSON.parse(localStorage.getItem('db_usuario'))).dados
+    var atual;
+    
+    usuarios.forEach(element => {
+        if(element.id == id){
+            atual= element
+        }
+    });
+    
+    $('.titulos').append(`${atual.nome}`)
+    $('.imagem-perfil'.append)(`
+        <img src="${atual.pictures.medium}" class="imagem-usuario">
+    `)
+    $('#detalhes').append(`
+    Categoria: ${atual.categoria}`)
+    $("#telefone").append(`
+    Contato: ${atual.telefone}`)
+    $("#email").append(`
+        Email: ${atual.email}
+    `)
+}
