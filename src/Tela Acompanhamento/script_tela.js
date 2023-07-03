5d73956b46a152c148e45c44f3aeeb21f5c8855f
Nicolas Henrique <nicolasfonseca5@hotmail.com>
2023-07-03 00:26:01 -0300

    at correcao

--- src/Tela Acompanhamento/script_tela.js
+++ src/Tela Acompanhamento/script_tela.js
@@ -1,341 +1,69 @@
-var pedidos = JSON.parse(localStorage.getItem('db_pedidos'))
+var pedidos = JSON.parse(localStorage.getItem('db_pedidos'));
 var usuarios = JSON.parse(localStorage.getItem('db_usuario'));
-var usuariologado = JSON.parse(sessionStorage.getItem('usuario'));
-var usuariodoador = [];
 var iguais = [];
-var usuariopedidos = [];
 
-if(usuariologado.tipo == "Beneficiario")
-{
+usuarios.forEach(elemento => {
     pedidos.forEach(element => {
-        if(usuariologado.nome == element.solicitante){
-            usuariopedidos.push(element)
+        if(elemento.nome == element.titulo){
+            iguais.push(elemento.endereco.logradouro)
         }
     });
-    
-    usuarios.forEach(elemento => {
-        usuariopedidos.forEach(element => {
-            if(elemento.nome == element.solicitante){
-                iguais.push(elemento.endereco.logradouro)
-            }
-        });
-    });
-}
+});
 
-if(usuariologado.tipo == "Doador")
-{
-    pedidos.forEach(element => {
-        if(usuariologado.nome == element.solicitado){
-            usuariopedidos.push(element)
-        }
-    });
-    
-    usuarios.forEach(elemento => {
-        usuariopedidos.forEach(element => {
-            if(elemento.nome == element.solicitado){
-                iguais.push(elemento.endereco.logradouro)
-            }
-        });
-    });
-}
 
 function exibirAcompanhamento() {
     let str = ''
     if (pedidos) {
-      for (let i = 0; i< usuariopedidos.length;i++) {
-        if(usuariopedidos[i].status == "processado"){
-            str += 
-            `<div class="frame-header">
-                <h5>PEDIDO EM NOME DE <span class="text-primary font-weight-bold">${usuariopedidos[i].solicitante}</span></h5>
-            </div>
-            <div class="frame-header">
-                <h5>Situação Do Pedido:<span class="text-primary font-weight-bold"> ${usuariopedidos[i].status}</span></h5>
-            </div>
-            </div>
-            <div class="row justify-content-between top">
-                <div class="progress-bar-container">
-                <div class="progress-bar-image icon-image">
-                    <img class="icon" style="color: black;" src="Tela Acompanhamento/Imagens/Pedido Processado colorido.png">
-                    <div class="d-flex flex-column">
-                    <p class="font-weight-bold">Pedido<br>Processado</p>
-                    </div>
-                </div>
-                <div class="progress-bar-image icon-image">
-                    <img class="icon" src="Tela Acompanhamento/Imagens/Pedido Pronto pra entrega.png">
-                    <div class="d-flex flex-column">
-                    <p class="font-weight-bold">Pedido<br>Pronto para entrega</p>
-                    </div>
-                </div>
-                <div class="progress-bar-image icon-image">
-                    <img class="icon" src="Tela Acompanhamento/Imagens/Pedido Enviado.png">
-                    <div class="d-flex flex-column">
-                    <p class="font-weight-bold">Pedido<br>Em Trânsito</p>
-                    </div>
-                </div>
-                <div class="progress-bar-image icon-image">
-                    <img class="icon" src="Tela Acompanhamento/Imagens/Pedido chegou.png">
-                    <div class="d-flex flex-column">
-                    <p class="font-weight-bold">Pedido<br>Chegou</p>
-                    </div>
-                </div>
+      for (let i = 0; i< pedidos.length;i++) {
+        str += 
+        `<div class="frame-header">
+            <h5>PEDIDO EM NOME DE <span class="text-primary font-weight-bold">${pedidos[i].titulo}</span></h5>
+        </div>
+        <div class="frame-header">
+            <h5>Situação Do Pedido:<span class="text-primary font-weight-bold"> ${pedidos[i].situacao}</span></h5>
+        </div>
+        </div>
+        <div class="row justify-content-between top">
+            <div class="progress-bar-container">
+            <div class="progress-bar-image icon-image">
+                <img class="icon" src="Tela Acompanhamento/Imagens/Pedido Processado.png">
+                <div class="d-flex flex-column">
+                <p class="font-weight-bold">Pedido<br>Processado</p>
                 </div>
             </div>
-            <div class="delivery-details">
-                <p><strong>Detalhes da Entrega:</strong></p>
-                <p>Transportadora: Nome da Transportadora</p>
-                <p>Data de Entrega: ${usuariopedidos[i].data}</p>
-                <p>Endereço: ${iguais[i]}</p>
-            </div>
-            <div class="row justify-content-center top">
-                <a href="CaixadeMensagem.html" class="btn btn-duvidas">Dúvidas sobre Entrega</a>
-                <a href="Meus-Pedidos.html" class="btn btn-ver">Ver Detalhes do Pedido</a>
-            </div>`;
-        }
-        if(usuariopedidos[i].status == "pronto"){
-            str += 
-            `<div class="frame-header">
-                <h5>PEDIDO EM NOME DE <span class="text-primary font-weight-bold">${usuariopedidos[i].solicitante}</span></h5>
-            </div>
-            <div class="frame-header">
-                <h5>Situação Do Pedido:<span class="text-primary font-weight-bold"> ${usuariopedidos[i].status}</span></h5>
-            </div>
-            </div>
-            <div class="row justify-content-between top">
-                <div class="progress-bar-container">
-                <div class="progress-bar-image icon-image">
-                    <img class="icon" src="Tela Acompanhamento/Imagens/Pedido Processado colorido.png">
-                    <div class="d-flex flex-column">
-                    <p class="font-weight-bold">Pedido<br>Processado</p>
-                    </div>
-                </div>
-                <div class="progress-bar-image icon-image">
-                    <img class="icon" src="Tela Acompanhamento/Imagens/Pedido Pronto pra entrega colorido.png">
-                    <div class="d-flex flex-column">
-                    <p class="font-weight-bold">Pedido<br>Pronto para entrega</p>
-                    </div>
-                </div>
-                <div class="progress-bar-image icon-image">
-                    <img class="icon" src="Tela Acompanhamento/Imagens/Pedido Enviado.png">
-                    <div class="d-flex flex-column">
-                    <p class="font-weight-bold">Pedido<br>Em Trânsito</p>
-                    </div>
-                </div>
-                <div class="progress-bar-image icon-image">
-                    <img class="icon" src="Tela Acompanhamento/Imagens/Pedido chegou.png">
-                    <div class="d-flex flex-column">
-                    <p class="font-weight-bold">Pedido<br>Chegou</p>
-                    </div>
-                </div>
+            <div class="progress-bar-image icon-image">
+                <img class="icon" src="Tela Acompanhamento/Imagens/Pedido Pronto pra entrega.png">
+                <div class="d-flex flex-column">
+                <p class="font-weight-bold">Pedido<br>Pronto para entrega</p>
                 </div>
             </div>
-            <div class="delivery-details">
-                <p><strong>Detalhes da Entrega:</strong></p>
-                <p>Transportadora: Nome da Transportadora</p>
-                <p>Data de Entrega: ${usuariopedidos[i].data}</p>
-                <p>Endereço: ${iguais[i]}</p>
-            </div>
-            <div class="row justify-content-center top">
-                <a href="CaixadeMensagem.html" class="btn btn-duvidas">Dúvidas sobre Entrega</a>
-                <a href="Meus-Pedidos.html" class="btn btn-ver">Ver Detalhes do Pedido</a>
-            </div>`;
-        }
-        if(usuariopedidos[i].status == "transito"){
-            str += 
-            `<div class="frame-header">
-                <h5>PEDIDO EM NOME DE <span class="text-primary font-weight-bold">${usuariopedidos[i].solicitante}</span></h5>
-            </div>
-            <div class="frame-header">
-                <h5>Situação Do Pedido:<span class="text-primary font-weight-bold"> ${usuariopedidos[i].status}</span></h5>
-            </div>
-            </div>
-            <div class="row justify-content-between top">
-                <div class="progress-bar-container">
-                <div class="progress-bar-image icon-image">
-                    <img class="icon" src="Tela Acompanhamento/Imagens/Pedido Processado colorido.png">
-                    <div class="d-flex flex-column">
-                    <p class="font-weight-bold">Pedido<br>Processado</p>
-                    </div>
-                </div>
-                <div class="progress-bar-image icon-image">
-                    <img class="icon" src="Tela Acompanhamento/Imagens/Pedido Pronto pra entrega colorido.png">
-                    <div class="d-flex flex-column">
-                    <p class="font-weight-bold">Pedido<br>Pronto para entrega</p>
-                    </div>
-                </div>
-                <div class="progress-bar-image icon-image">
-                    <img class="icon" src="Tela Acompanhamento/Imagens/Pedido Enviado colorido.png">
-                    <div class="d-flex flex-column">
-                    <p class="font-weight-bold">Pedido<br>Em Trânsito</p>
-                    </div>
-                </div>
-                <div class="progress-bar-image icon-image">
-                    <img class="icon" src="Tela Acompanhamento/Imagens/Pedido chegou.png">
-                    <div class="d-flex flex-column">
-                    <p class="font-weight-bold">Pedido<br>Chegou</p>
-                    </div>
-                </div>
+            <div class="progress-bar-image icon-image">
+                <img class="icon" src="Tela Acompanhamento/Imagens/Pedido Enviado.png">
+                <div class="d-flex flex-column">
+                <p class="font-weight-bold">Pedido<br>Em Trânsito</p>
                 </div>
             </div>
-            <div class="delivery-details">
-                <p><strong>Detalhes da Entrega:</strong></p>
-                <p>Transportadora: Nome da Transportadora</p>
-                <p>Data de Entrega: ${usuariopedidos[i].data}</p>
-                <p>Endereço: ${iguais[i]}</p>
-            </div>
-            <div class="row justify-content-center top">
-                <a href="CaixadeMensagem.html" class="btn btn-duvidas">Dúvidas sobre Entrega</a>
-                <a href="Meus-Pedidos.html" class="btn btn-ver">Ver Detalhes do Pedido</a>
-            </div>`;
-        }
-        if(usuariopedidos[i].status == "chegou"){
-            str += 
-            `<div class="frame-header">
-                <h5>PEDIDO EM NOME DE <span class="text-primary font-weight-bold">${usuariopedidos[i].solicitante}</span></h5>
-            </div>
-            <div class="frame-header">
-                <h5>Situação Do Pedido:<span class="text-primary font-weight-bold"> ${usuariopedidos[i].status}</span></h5>
-            </div>
-            </div>
-            <div class="row justify-content-between top">
-                <div class="progress-bar-container">
-                <div class="progress-bar-image icon-image">
-                    <img class="icon" src="Tela Acompanhamento/Imagens/Pedido Processado colorido.png">
-                    <div class="d-flex flex-column">
-                    <p class="font-weight-bold">Pedido<br>Processado</p>
-                    </div>
-                </div>
-                <div class="progress-bar-image icon-image">
-                    <img class="icon" src="Tela Acompanhamento/Imagens/Pedido Pronto pra entrega colorido.png">
-                    <div class="d-flex flex-column">
-                    <p class="font-weight-bold">Pedido<br>Pronto para entrega</p>
-                    </div>
-                </div>
-                <div class="progress-bar-image icon-image">
-                    <img class="icon" src="Tela Acompanhamento/Imagens/Pedido Enviado colorido.png">
-                    <div class="d-flex flex-column">
-                    <p class="font-weight-bold">Pedido<br>Em Trânsito</p>
-                    </div>
-                </div>
-                <div class="progress-bar-image icon-image">
-                    <img class="icon" src="Tela Acompanhamento/Imagens/Pedido chegou colorido.png">
-                    <div class="d-flex flex-column">
-                    <p class="font-weight-bold">Pedido<br>Chegou</p>
-                    </div>
-                </div>
+            <div class="progress-bar-image icon-image">
+                <img class="icon" src="Tela Acompanhamento/Imagens/Pedido chegou.png">
+                <div class="d-flex flex-column">
+                <p class="font-weight-bold">Pedido<br>Chegou</p>
                 </div>
             </div>
-            <div class="delivery-details">
-                <p><strong>Detalhes da Entrega:</strong></p>
-                <p>Transportadora: Nome da Transportadora</p>
-                <p>Data de Entrega: ${usuariopedidos[i].data}</p>
-                <p>Endereço: ${iguais[i]}</p>
             </div>
-            <div class="row justify-content-center top">
-                <a href="CaixadeMensagem.html" class="btn btn-duvidas">Dúvidas sobre Entrega</a>
-                <a href="Meus-Pedidos.html" class="btn btn-ver">Ver Detalhes do Pedido</a>
-            </div>`;
-        }
-        if(usuariopedidos[i].status == "Pedido Realizado"){
-            str += 
-            `<div class="frame-header">
-                <h5>PEDIDO EM NOME DE <span class="text-primary font-weight-bold">${usuariopedidos[i].solicitante}</span></h5>
-            </div>
-            <div class="frame-header">
-                <h5>Situação Do Pedido:<span class="text-primary font-weight-bold"> ${usuariopedidos[i].status}</span></h5>
-            </div>
-            </div>
-            <div class="row justify-content-between top">
-                <div class="progress-bar-container">
-                <div class="progress-bar-image icon-image">
-                    <img class="icon" src="Tela Acompanhamento/Imagens/Pedido Processado.png">
-                    <div class="d-flex flex-column">
-                    <p class="font-weight-bold">Pedido<br>Processado</p>
-                    </div>
-                </div>
-                <div class="progress-bar-image icon-image">
-                    <img class="icon" src="Tela Acompanhamento/Imagens/Pedido Pronto pra entrega.png">
-                    <div class="d-flex flex-column">
-                    <p class="font-weight-bold">Pedido<br>Pronto para entrega</p>
-                    </div>
-                </div>
-                <div class="progress-bar-image icon-image">
-                    <img class="icon" src="Tela Acompanhamento/Imagens/Pedido Enviado.png">
-                    <div class="d-flex flex-column">
-                    <p class="font-weight-bold">Pedido<br>Em Trânsito</p>
-                    </div>
-                </div>
-                <div class="progress-bar-image icon-image">
-                    <img class="icon" src="Tela Acompanhamento/Imagens/Pedido chegou.png">
-                    <div class="d-flex flex-column">
-                    <p class="font-weight-bold">Pedido<br>Chegou</p>
-                    </div>
-                </div>
-                </div>
-            </div>
-            <div class="delivery-details">
-                <p><strong>Detalhes da Entrega:</strong></p>
-                <p>Transportadora: Nome da Transportadora</p>
-                <p>Data de Entrega: ${usuariopedidos[i].data}</p>
-                <p>Endereço: ${iguais[i]}</p>
-            </div>
-            <div class="row justify-content-center top">
-                <a href="CaixadeMensagem.html" class="btn btn-duvidas">Dúvidas sobre Entrega</a>
-                <a href="Meus-Pedidos.html" class="btn btn-ver">Ver Detalhes do Pedido</a>
-            </div>`;
-        }
-        if(usuariopedidos[i].status == ""){
-            usuariopedidos[i].status = "Pedido Invalido"
-            str += 
-            `<div class="frame-header">
-                <h5>PEDIDO EM NOME DE <span class="text-primary font-weight-bold">${usuariopedidos[i].solicitante}</span></h5>
-            </div>
-            <div class="frame-header">
-                <h5>Situação Do Pedido:<span class="text-primary font-weight-bold"> ${usuariopedidos[i].status}</span></h5>
-            </div>
-            </div>
-            <div class="row justify-content-between top">
-                <div class="progress-bar-container">
-                <div class="progress-bar-image icon-image">
-                    <img class="icon" src="Tela Acompanhamento/Imagens/Pedido Processado colorido.png">
-                    <div class="d-flex flex-column">
-                    <p class="font-weight-bold">Pedido<br>Processado</p>
-                    </div>
-                </div>
-                <div class="progress-bar-image icon-image">
-                    <img class="icon" src="Tela Acompanhamento/Imagens/Pedido Pronto pra entrega colorido.png">
-                    <div class="d-flex flex-column">
-                    <p class="font-weight-bold">Pedido<br>Pronto para entrega</p>
-                    </div>
-                </div>
-                <div class="progress-bar-image icon-image">
-                    <img class="icon" src="Tela Acompanhamento/Imagens/Pedido Enviado colorido.png">
-                    <div class="d-flex flex-column">
-                    <p class="font-weight-bold">Pedido<br>Em Trânsito</p>
-                    </div>
-                </div>
-                <div class="progress-bar-image icon-image">
-                    <img class="icon" src="Tela Acompanhamento/Imagens/Pedido chegou colorido.png">
-                    <div class="d-flex flex-column">
-                    <p class="font-weight-bold">Pedido<br>Chegou</p>
-                    </div>
-                </div>
-                </div>
-            </div>
-            <div class="delivery-details">
-                <p><strong>Detalhes da Entrega:</strong></p>
-                <p>Transportadora: Nome da Transportadora</p>
-                <p>Data de Entrega: ${usuariopedidos[i].data}</p>
-                <p>Endereço: ${iguais[i]}</p>
-            </div>
-            <div class="row justify-content-center top">
-                <a href="CaixadeMensagem.html" class="btn btn-duvidas">Dúvidas sobre Entrega</a>
-                <a href="Meus-Pedidos.html" class="btn btn-ver">Ver Detalhes do Pedido</a>
-            </div>`;
-        }
+        </div>
+        <div class="delivery-details">
+            <p><strong>Detalhes da Entrega:</strong></p>
+            <p>Transportadora: Nome da Transportadora</p>
+            <p>Data de Entrega: ${pedidos[i].data_entrega}</p>
+            <p>Endereço: ${iguais[i]}</p>
+        </div>
+        <div class="row justify-content-center top">
+            <a href="CaixadeMensagem.html" class="btn btn-duvidas">Dúvidas sobre Entrega</a>
+            <a href="Meus-Pedidos.html" class="btn btn-ver">Ver Detalhes do Pedido</a>
+        </div>`;
       }
       document.getElementById('tela').innerHTML = str;
     }
 }
 
 window.onload = exibirAcompanhamento;
-
--- src/Tela-Pedidos/app_pedidos.js
+++ src/Tela-Pedidos/app_pedidos.js
@@ -1,58 +1,6 @@
+let toggle = document.querySelector('.toggle');
+let sideBar = document.querySelector('.sidebar-container');
 
-var usuariologado = JSON.parse(sessionStorage.getItem('usuario'));
-
-if(usuariologado.tipo == "Doador"){
-  let str = 
-  `<p class="cabecalho" style="text-align: center;">
-    <h2 class="cabecalho-titulo"> Alterar Pedidos</h2>
-  </p>
-  <form id="pedido-form">
-    <label for="titulo">Nome:</label>
-    <select name="" id="select-nomes">
-    
-    </select>
-    <label for="titulo">Situação Pedido:</label>
-    <select id="onde">
-      <option value="processado">processado</option>
-      <option value="pronto">pronto</option>
-      <option value="transito">transito</option>
-      <option value="chegou">chegou</option>
-    </select>
-    <button style="margin-top: 20px" type="submit" id="alterar">Alterar Situação</button>
-  </form>
-  <p class="cabecalho" style="text-align: center;">
-    <h2 class="cabecalho-titulo"> Pedidos Realizados</h2>
-  </p>
-  </div>
-  <!-- CARDS DE PEDIDOS -->
-  <div id="div-content">
-    <!--CARD DE PEDIDO-->
-    <div class="card container-fluid mb-3">
-      <div id="tela">
-
-      </div>
-    </div>
-    <!--FIM DO CARD DE PEDIDO-->
-  </div>
-  `
-  document.getElementById('tela2').innerHTML = str;
-}
-
-if(usuariologado.tipo == "Beneficiario"){
-  let str =
-  `<p class="cabecalho" style="text-align: center;">
-  <h2 class="cabecalho-titulo"> Pedidos Realizados</h2>
-  </p>
-  </div>
-  <!-- CARDS DE PEDIDOS -->
-  <div id="div-content">
-    <!--CARD DE PEDIDO-->
-    <div class="card container-fluid mb-3">
-      <div id="tela">
-
-      </div>
-    </div>
-    <!--FIM DO CARD DE PEDIDO-->
-  </div>`
-  document.getElementById('tela2').innerHTML = str;
-}
+toggle.addEventListener('click', () => {
+  sideBar.classList.toggle('toggle-function');
+});
