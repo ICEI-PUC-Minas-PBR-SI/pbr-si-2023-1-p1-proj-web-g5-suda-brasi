39897a6d5d6d0ef6569f9aee53fa5b2f1062cef0
GabrielValgas <gabrielsantosre98@gmail.com>
2023-07-03 00:15:49 -0300

    commitando

--- src/Tela-Pedidos/CadastroPedidos.js
+++ src/Tela-Pedidos/CadastroPedidos.js
@@ -1,67 +1,28 @@
-var usuarios = (JSON.parse(localStorage.getItem('db_usuarios')))
-
-function opcoes(){
-  let str = ''
-  for (let i = 1; i < usuarios.length;i++) {
-    str += 
-    `
-      <option value="${usuarios[i].nome}">${usuarios[i].nome}</option>
-    `
-  }
-  document.getElementById('select-nomes').innerHTML = str;
-}
-
-opcoes()
-
 
 // Script.js
-const form = document.querySelector('#pedido-form');
+
+var doadorpedidos = [];
+var usuariopedidos = [];
 
 
-function salvarPedido(event) {
-  event.preventDefault();
 
-  const tituloInput = document.getElementById('select-nomes');
-  const descricaoInput = document.getElementById('descricao-pedido');
-  const situacaoInput = document.getElementById('situacao-pedido');
+var pedidos = JSON.parse(localStorage.getItem('db_pedidos'));
 
-  const pedido = {
-    id: new Date().getTime(),
-    titulo: tituloInput.value,
-    descricao: descricaoInput.value,
-    situacao: situacaoInput.value,
-    data_envio: new Date().toLocaleString(),
-    data_entrega: new Date().toLocaleString()
-  };
-
-  let pedidos = JSON.parse(sessionStorage.getItem('pedidos'));
-  if (!pedidos) {
-    pedidos = [];
+pedidos.forEach(element => {
+  if(usuariologado.nome == element.solicitante){
+      usuariopedidos.push(element)
   }
-
-  pedidos.push(pedido);
-  sessionStorage.setItem('pedidos', JSON.stringify(pedidos));
-
-  tituloInput.value = '';
-  descricaoInput.value = '';
-  situacaoInput.value='';
-
-  exibirPedidos();
-}
+});
 
 function exibirPedidos() {
   let str = ''
-
-  
-  var pedidos = JSON.parse(sessionStorage.getItem('pedidos'));
-  if (pedidos) {
-    pedidos.forEach((pedido) => {
+  if (usuariologado.tipo == "Beneficiario") {
+    usuariopedidos.forEach((usuariopedidos) => {
       str += 
       `<div class="row">
             <div id="topo-card">
-                <span class="texto-card">Situação: ${pedido.situacao}</span>
-                <span class="texto-card">Data do Pedido: ${pedido.data_envio}</span>
-                <span class="texto-card">Data de Entrega: ${pedido.data_entrega}</span>
+                <span class="texto-card">Situação: ${usuariopedidos.status}</span>
+                <span class="texto-card">Data de Entrega: ${usuariopedidos.data}</span>
             </div>
             </div>
             <div class="row">
@@ -70,15 +31,85 @@ function exibirPedidos() {
             </div>
             <div class="col-md-8">
                 <div class="card-body">
-                <h5 class="card-title">Nome: ${pedido.titulo}</h5>
-                <span class="card-text">Descrição: ${pedido.descricao}</span>
+                <h5 class="card-title">Nome: ${usuariopedidos.solicitante}</h5>
+                <span class="card-text">Descrição: ${usuariopedidos.descricao}</span>
                 </div>
+            </div>
+            <div>
+              <a href="Acompanhamento.html"><button id="btn_Acompanhar">Acompanhar Pedido</button></a>
             </div>
         </div>`;
     });
     document.getElementById('tela').innerHTML = str;
   }
 }
+pedidos.forEach(element => {
+  if(usuariologado.nome == element.solicitado){
+      doadorpedidos.push(element)
+  }
+});
+function exibirPedidosDoador() {
+  let str = ''
+  if (usuariologado.tipo == "Doador") {
+    doadorpedidos.forEach((doadorpedidos) => {
+      str += 
+      `<div class="row">
+            <div id="topo-card">
+                <span class="texto-card">Situação: ${doadorpedidos.status}</span>
+                <span class="texto-card">Data de Entrega: ${doadorpedidos.data}</span>
+            </div>
+            </div>
+            <div class="row">
+            <div class="col-md-3">
+                <img src="https://cdn-icons-png.flaticon.com/512/2904/2904948.png" class="imagem-usuario">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                <h5 class="card-title">Nome: ${doadorpedidos.solicitante}</h5>
+                <span class="card-text">Descrição: ${doadorpedidos.descricao}</span>
+                </div>
+            </div>
+            <div>
+              <a href="Acompanhamento.html"><button id="btn_Acompanhar">Acompanhar Pedido</button></a>
+            </div>
+        </div>`;
+    });
+    document.getElementById('tela').innerHTML = str;
+  }
+}
+function opcoes(){
+  if(usuariologado.tipo == "Doador")
+  {
+    let str = ''
+    for (let i = 0; i < doadorpedidos.length;i++) {
+      str += 
+      `
+        <option id="valor" value="${doadorpedidos[i].solicitante}">${doadorpedidos[i].solicitante}</option>
+      `
+    }
+    document.getElementById('select-nomes').innerHTML = str;
+  }
+}
 
-form.addEventListener('submit', salvarPedido);
+function alterarinfo()
+{
+  if(usuariologado.tipo == "Doador")
+  {
+    var nomesolicitante = document.getElementById('select-nomes').value
+    var onde = document.getElementById('onde').value
+    pedidos.forEach((pedidos) => {
+      if(pedidos.solicitante == nomesolicitante){
+        pedidos.status = onde
+      }
+    })
+    localStorage.setItem('db_pedidos' , JSON.stringify(pedidos))
+  }
+}
+if(usuariologado.tipo == "Doador"){
+  var botaotop = document.getElementById('alterar')
+  botaotop.addEventListener('click' , alterarinfo)
+}
+opcoes();
 exibirPedidos();
+exibirPedidosDoador();
+
